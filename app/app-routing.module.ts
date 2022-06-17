03a4dac5f68362ff5b5c0859a1d4e3fb8cde0da7
DenisAGS <denissearelyg@gmail.com>
2022-06-17 15:15:46 -0500

    update

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -3,21 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { PrincipalComponent} from './principal/principal.component';
 import { ProductosComponent } from './productos/productos.component';
 import { InformacionComponent } from './informacion/informacion.component';
 import { PruebaComponent } from './prueba/prueba.component';
 
 const routes: Routes = [  
 {
-  path: '',
-  component: PrincipalComponent,
-  },
-  {
-    path:'principal',
-    component: PrincipalComponent,
-  },
-  {
     path:'productos',
     component: ProductosComponent,
   },
--- app/app.module.ts
+++ app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PrincipalComponent } from './principal/principal.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -14,7 +13,6 @@ import { PruebaComponent } from './prueba/prueba.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PrincipalComponent,
     ProductosComponent,
     InformacionComponent,
     PruebaComponent
